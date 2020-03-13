32c40ce3b564b883b6951733d2de9f9a17aa58f6
23paulka <59451406+23paulka@users.noreply.github.com>
2020-03-13 13:41:23 -0400

    Add files via upload

---
+++ paulson_unit1_practice.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# PART A
+print("Welcome to Information Technology\n\nProgramming and\nWeb Development")
+print("")
+print(" _       ___________\n| |     / /_  __/   |\n| | /| / / / / / /| |\n| |/ |/ / / / / ___ |\n|__/|__/ /_/ /_/  |_|")
+
+
+
+
+
+#PART B
+print("A lightning flash:\nbetween the forest trees\nI have seen water.\n\t-Shiki")
+
+
+print("**********\n*        *\n* MR. G  *\n* RM 119 *\n*        *\n**********")
+
+
+print("Computer Science is no more about computers\nthan astronomy is about telescopes\n\t-Edsger W. Dijkstra")
+
+
+
+
+# PART C
+user_name= input("What is your name?")
+favorite_animal= input("What is your Favorite Animal?")
+print("Hello", user_name,".Your favorite animal is the", favorite_animal,".")
+
+
+number =input("Whats your favorite number?")
+print(number*3)
+
+
+number_1=int(input("test score #1?"))
+number_2=int(input("test scroce #2?"))
+number_3=int(input("test score #3?"))
+total=number_1+number_2+number_3
+print(total/3)
+
+
+word_1=input("enter a word:")
+word_2=input("enter a word sir:")
+word_3=input("can you please just put a word?")
+print(word_3 ,word_2 ,word_1)
+
+
+length=int(input("Enter the length:"))
+width=int(input("Enter the width:"))
+print("  ",length,"\n\n")
+print(" ------")
+print("|","    ","|")
+print("|","    ","|",width)
+print("|","    ","|")
+print(" ------")
+print("\n\nArea=",length*width)
+
+
+
+
+# PART D
+w_1=input("enter a word:")
+w_2=input("enter another word:")
+print(w_1,w_2)
+
+
+First_name=input("enter your first name:")
+Last_name=input("enter your last name:")
+print(Last_name,First_name)
+
+
+num_1=input("enter a number:")
+print(num_1*8)
+
+
+Number_6=int(input("enter a number"))
+print(Number_6*12)
+
+
+
+
+# PART E
+a=input("Enter an animal:")
+s=input("Enter a sound")
+e= "E-I-E-I-O"
+print("Old Macdonald had a farm,"+e)
+print("And on his farm he had a"+a","+e)
+print("With a"+a+"-"+a+"here and a"+s+s"-"+s+"there")
+print("Here a"+s+"there a"+s)
+print("Everywhere a"+s+"-"+a)
+print("Old Macdonald had a farm,"+e)
